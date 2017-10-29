0267fae4eec68aa1680069f4d3bd46cd4183ab45
meamuri <meammf@yandex.ru>
2017-10-29 16:02:31 +0300

    new output format

--- lab1_isPrime.lisp
+++ lab1_isPrime.lisp
@@ -1,6 +1,7 @@
 #!/usr/bin/sbcl --script
-; 8. Проверить, является ли указанное число простым.
+;; 8. Проверить, является ли указанное число простым.
 
+;; function which checks that number is prime
 (defun isprime (x)
   (if (< x 2)
 	  (return-from isprime nil)
@@ -13,9 +14,18 @@
     (return-from isprime t)
 )
 
-(print (isprime 7))
-(print (isprime 13))
-(print (isprime 19))
-(print (isprime 25))
-(print (isprime 40))
-(print (isprime 97))
+;; Функция, проверяющая, является ли x корнем многочлена с коэффициентами a, b, c
+;; Если коэффициенты многочлена не заданы, будет взят многочлен x^2 - 2*x + 1
+(defun prime-service (x)
+  (if (equal (isprime x) nil)
+		(format t "~D is prime number ~%" x)
+		(format t "~D is not prime number ~%" x)    
+	)
+)
+
+(prime-service 7)
+(prime-service 13)
+(prime-service 19)
+(prime-service 25)
+(prime-service 40)
+(prime-service 97)
