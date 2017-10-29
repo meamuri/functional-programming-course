b491fad230dbce66d48b9db74f73889225c8b646
meamuri <meammf@yandex.ru>
2017-10-29 16:11:53 +0300

    print service for task 2

--- lab2_treeDepth.lisp
+++ lab2_treeDepth.lisp
@@ -3,17 +3,21 @@
 (defun maxdepth (l)
   (cond ((null l) 0)
         ((atom l) 0)
-        (t (+ 1 (max (maxdepth (cadr l)) (maxdepth (caddr l)))))))
+        (t (+ 1 (max (maxdepth (cadr l)) (maxdepth (caddr l)))))
+  )
+)
+
+(defun depth-service (tree)
+		(format t "~D is depth of tree ~%" (maxdepth tree))		
+)
 
 ;;               A
 ;;             /   \
 ;;            B      C
 ;;                 /   \
 ;;                 D    E
-(print 
-  (maxdepth 
-    '(A (B) (C (D) (E)))
-  )
+(depth-service 
+  '(A (B) (C (D) (E)))
 )
 
 ;;               A
@@ -23,19 +27,15 @@
 ;;                 D    E
 ;;                       \
 ;;                        F
-(print 
-  (maxdepth 
-    '(A (B) (C (D) (E (F))))
-  )
+(depth-service 
+  '(A (B) (C (D) (E (F))))
 )
 
 ;;               A
 ;;                 \
 ;;                   B              
-(print 
-  (maxdepth 
-    '(A (B))
-  )
+(depth-service 
+  '(A (B))
 )
 
 ;;
@@ -44,12 +44,9 @@
 ;;   B   C
 ;;  / \  |
 ;; D   E F
-(print 
-  (maxdepth 
-    '(A (B (D) (E)) (C (F)))
-  )
+(depth-service 
+  '(A (B (D) (E)) (C (F)))
 )
-
 
 ;; http://shrager.org/llisp/13.html
 ;; a bit about trees
