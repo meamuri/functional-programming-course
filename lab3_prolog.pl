52ac2a2427142c61b77c3f56f77f229c51df91a9
meamuri <meammf@yandex.ru>
2017-10-30 01:11:01 +0300

    reverse implementation

--- lab3_prolog.pl
+++ lab3_prolog.pl
@@ -4,3 +4,12 @@
 :- use_module(library(lists)).
 
 is_sym(L) :- reverse(L,L).
+
+my_reverse(List, Rev) :-
+        my_reverse(List, Rev, []).
+
+my_reverse([], L, L).
+my_reverse([H|T], L, SoFar) :-
+        my_reverse(T, L, [H|SoFar]).
+
+is_rev(List) :- my_reverse(List, X), List == X.
