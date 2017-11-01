ee21ed82d0cc297663af57e046436e683da3d642
meamuri <meammf@yandex.ru>
2017-11-01 10:33:47 +0300

    new isprime function

--- lab1_isPrime.lisp
+++ lab1_isPrime.lisp
@@ -2,29 +2,22 @@
 ;; 8. Проверить, является ли указанное число простым.
 
 ;; function which checks that number is prime
-(defun isprime (x)
-  (if (< x 2)
-	  (return-from isprime nil)
-	;; ceiling and fceiling produce a quotient that has been truncated toward positive infinity; 
-	;; that is, the quotient represents the smallest mathematical integer 
-	;; that is not smaller than the mathematical result.
-	(dotimes (i (+ (ceiling (sqrt x)) 1))
-	  (if (> i 1)
-		  (if (< i x)
-			  (if (equal (mod x i) 0)
-				  (return-from isprime nil))))))
-
-    (return-from isprime t)
-)
+(defun isprime (n &optional (d (- n 1))) 
+	(if (= n 1)
+		(return-from isprime nil))
+  (or (= d 1)
+      (and (/= (rem n d) 0)
+           (isprime  n (- d 1)))))
 
 ;; service provides userfriendly output
 (defun prime-service (x)
-  (if (equal (isprime x) nil)
+  (if (equal (isprime x) t)
 		(format t "~D is prime number ~%" x)
 		(format t "~D is not prime number ~%" x)    
 	)
 )
 
+(prime-service 1)
 (prime-service 7)
 (prime-service 13)
 (prime-service 19)
